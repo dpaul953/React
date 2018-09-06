9ce364021a6d14a61d0c648f4b759beea6f739cd
Dattatreyo Paul <dattatreyop953@IN.nam.ad.pwcinternal.com>
2018-09-06 18:12:01 +0530

    changes made for courdes page

---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,12 @@
+import React, {ProtoTypes} from 'react';
+class CoursesPage extends React.Component {
+    render(){
+        return (
+            <div>
+              <h1>courses</h1>
+            </div>
+        );
+    }
+}
+
+export default CoursesPage;
